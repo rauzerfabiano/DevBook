7f133e5b5bc310a0104872fc1cfa5ca1f0a23e89
Rauzer <fabiano.rauzer@egsys.com.br>
2022-06-17 20:44:09 -0300

    Criada string de conexão com o banco e chamando na func main

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -8,9 +9,11 @@ import (
 )
 
 func main() {
+	config.Carregar()
+
 	fmt.Println("Rodando API!")
 
 	r := router.Gerar()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
