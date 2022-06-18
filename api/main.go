3fcaf6ef16c8ed35f1263157a05d834255239722
Rauzer <fabiano.rauzer@egsys.com.br>
2022-06-17 21:18:16 -0300

    Abrindo a conexão com o banco de dados

--- api/main.go
+++ api/main.go
@@ -11,9 +11,9 @@ import (
 func main() {
 	config.Carregar()
 
-	fmt.Println("Rodando API!")
-
 	r := router.Gerar()
+
+	fmt.Printf("Escutando na porta %d\n", config.Porta)
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
