5ad965faa75bb243eeecb0c09b0321bb947f51ab
Rauzer <fabiano.rauzer@egsys.com.br>
2022-06-27 08:12:57 -0300

    Validação de token

--- api/src/router/rotas/usuarios.go
+++ api/src/router/rotas/usuarios.go
@@ -17,7 +17,7 @@ var rotasUsuarios = []Rota{
 		URI:                "/usuarios",
 		Metodo:             http.MethodGet,
 		Funcao:             controllers.BuscarUsuarios,
-		RequerAutenticacao: false,
+		RequerAutenticacao: true,
 	},
 	{
 		URI:                "/usuarios/{usuarioId}",
