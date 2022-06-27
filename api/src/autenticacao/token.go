c2224c82345a58654b7d4db955be1ddcc7999ccb
Rauzer <fabiano.rauzer@egsys.com.br>
2022-06-27 09:07:21 -0300

    Criada validação para impedir um usuário de editar dados de outro

--- api/src/autenticacao/token.go
+++ api/src/autenticacao/token.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -34,6 +35,26 @@ func ValidarToken(r *http.Request) error {
 	}
 
 	return errors.New("token inválido")
+}
+
+// ExtrairUsuarioID retorna o usuárioID que está salvo no token
+func ExtrairUsuarioID(r *http.Request) (uint64, error) {
+	tokenString := extrairToken(r)
+	token, erro := jwt.Parse(tokenString, retornarChaveDeVerificacao)
+	if erro != nil {
+		return 0, erro
+	}
+
+	if permissoes, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		usuarioID, erro := strconv.ParseUint(fmt.Sprintf("%.0f", permissoes["usuarioId"]), 10, 64)
+		if erro != nil {
+			return 0, erro
+		}
+
+		return usuarioID, nil
+	}
+
+	return 0, errors.New("token inválido")
 }
 
 func extrairToken(r *http.Request) string {
