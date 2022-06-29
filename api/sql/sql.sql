65e960d4fd4498c100fbdc22a9d9cff35aaaf68b
Rauzer <fabiano.rauzer@egsys.com.br>
2022-06-29 09:03:27 -0300

    Criação do modelo de publicações e sql para criar a tabela

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
 
-DROP TABLE IF EXISTS usuarios;
+DROP TABLE IF EXISTS publicacoes;
 DROP TABLE IF EXISTS seguidores;
+DROP TABLE IF EXISTS usuarios;
 
 CREATE TABLE usuarios(
     id int AUTO_INCREMENT primary key,
@@ -25,4 +26,15 @@ CREATE TABLE seguidores(
     ON DELETE CASCADE,
 
     PRIMARY KEY (usuario_id, seguidor_id)
+)ENGINE=InnoDB;
+
+CREATE TABLE publicacoes(
+    id int auto_increment primary key,
+    titulo varchar(50) NOT NULL,
+    conteudo varchar(300) NOT NULL,
+    autor_id int not null, 
+    FOREIGN KEY (autor_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+
+    curtidas int default 0,
+    criadaEm timestamp default current_timestamp()
 )ENGINE=InnoDB;
