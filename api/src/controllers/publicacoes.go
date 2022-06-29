4d78e6062cc3c7d64a4f1241c42c42992d3bbfe7
Rauzer <fabiano.rauzer@egsys.com.br>
2022-06-29 09:21:21 -0300

    Criado os métodos na controller e configuração da rota

---
+++ api/src/controllers/publicacoes.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+// CriarPublicacao adiciona uma nova publicação no banco de dados
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// BuscarPublicacoes traz as publicações que apareceriam no feed do usuário
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// BuscarPublicacao traz uma única publicação
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// AtualizarPublicacao altera os dados de uma publicação
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// DeletarPublicacao exclui os dados de uma publicação
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
