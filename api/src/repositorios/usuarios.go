508ab90b60b7014f1bcb55a8800c4113803949d3
Rauzer <fabiano.rauzer@egsys.com.br>
2022-06-29 07:06:06 -0300

    Criada a rota e a lógica para buscar quem um usuário está seguindo

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -183,3 +183,27 @@ func (repositorio Usuarios) BuscarSeguidores(usuarioID uint64) ([]modelos.Usuari
 
 	return usuarios, nil
 }
+
+// BuscarSeguindo traz todos os usuários que um determinado usuário está seguindo
+func (repositorio Usuarios) BuscarSeguindo(usuarioID uint64) ([]modelos.Usuario, error) {
+	linhas, erro := repositorio.db.Query(`
+	select u.id, u.nome, u.nick, u.email, u.criadoEm
+	from usuarios u inner join seguidores s on u.id = s.usuario_id where s.seguidor_id = ?
+	`, usuarioID)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var usuarios []modelos.Usuario
+
+	for linhas.Next() {
+		var usuario modelos.Usuario
+		if erro = linhas.Scan(&usuario.ID, &usuario.Nome, &usuario.Nick, &usuario.Email, &usuario.CriadoEm); erro != nil {
+			return nil, erro
+		}
+		usuarios = append(usuarios, usuario)
+	}
+
+	return usuarios, nil
+}
