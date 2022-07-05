53a07692a9378985ca14ce393c4f6b1768bc22ca
Rauzer <fabiano.rauzer@egsys.com.br>
2022-07-05 08:31:53 -0300

    Criada rota, método e estilização da página de criar usuário

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -9,3 +9,8 @@ import (
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
+
+// CarregarPaginaDeCadastroDeUsuario vai renderizar a tela de cadastro de usuário
+func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
+	utils.ExecutarTemplate(w, "cadastro.html", nil)
+}
--- webapp/src/router/rotas/rotas.go
+++ webapp/src/router/rotas/rotas.go
@@ -17,6 +17,7 @@ type Rota struct {
 // Configurar coloca todas as rotas dentro do router
 func Configurar(router *mux.Router) *mux.Router {
 	rotas := rotasLogin
+	rotas = append(rotas, rotasUsuarios...)
 
 	for _, rota := range rotas {
 		router.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
