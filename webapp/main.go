9712434d381118e7bc7b3226208b3599893bf353
Rauzer <fabiano.rauzer@egsys.com.br>
2022-07-04 23:03:50 -0300

    Criação do webapp e do router

---
+++ webapp/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"webapp/src/router"
+)
+
+func main() {
+	fmt.Println("Rodando WebApp!")
+
+	r := router.Gerar()
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
