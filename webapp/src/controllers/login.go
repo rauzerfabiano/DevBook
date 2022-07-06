bf7e9b52f38bcc06d002434d86ae44f1e9e906e2
Rauzer <fabiano.rauzer@egsys.com.br>
2022-07-06 07:43:29 -0300

    Criação da lógica para realizar login

---
+++ webapp/src/controllers/login.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"webapp/src/respostas"
+)
+
+// FazerLogin utiliza o e-mail e senha do usuário para autenticar na aplicação
+func FazerLogin(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	usuario, erro := json.Marshal(map[string]string{
+		"email": r.FormValue("email"),
+		"senha": r.FormValue("senha"),
+	})
+
+	if erro != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	response, erro := http.Post("http://localhost:5000/login", "application/json", bytes.NewBuffer(usuario))
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	token, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(response.StatusCode, string(token))
+}
