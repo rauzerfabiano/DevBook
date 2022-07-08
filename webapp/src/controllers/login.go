86ddd3cf16a5093ab1ed783c140ad0af059bbc98
Rauzer <fabiano.rauzer@egsys.com.br>
2022-07-08 07:34:13 -0300

    Comentando a rota dinâmica, pois deu erro

--- webapp/src/controllers/login.go
+++ webapp/src/controllers/login.go
@@ -3,9 +3,7 @@ package controllers
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"webapp/src/config"
 	"webapp/src/modelos"
 	"webapp/src/respostas"
 )
@@ -24,8 +22,8 @@ func FazerLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	url := fmt.Sprintf("%s/login", config.APIURL)
-	response, erro := http.Post(url, "application/json", bytes.NewBuffer(usuario))
+	// url := fmt.Sprintf("%s/login", config.APIURL)
+	response, erro := http.Post("http://localhost:5000/login", "application/json", bytes.NewBuffer(usuario))
 	if erro != nil {
 		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
 		return
