f7aa62ef8b839480429b44fac01c6c779d923a71
Rauzer <fabiano.rauzer@egsys.com.br>
2022-07-13 07:08:17 -0300

    Criada a funcionalidade de criar publicações

--- webapp/src/router/rotas/rotas.go
+++ webapp/src/router/rotas/rotas.go
@@ -20,6 +20,7 @@ func Configurar(router *mux.Router) *mux.Router {
 	rotas := rotasLogin
 	rotas = append(rotas, rotasUsuarios...)
 	rotas = append(rotas, rotaPaginaPrincipal)
+	rotas = append(rotas, rotasPublicacoes...)
 
 	for _, rota := range rotas {
 
